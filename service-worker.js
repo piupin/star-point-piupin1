2113a57e6075579cb48ad75649467465c0f2ab17
piupin <piupin0000@gmail.com>
2025-08-15 01:50:05 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,51 @@
+const CACHE_NAME = 'stars-cache-v2';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
+];
+
+// Install Service Worker and cache files
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('star-cache-v2').then((cache) => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './splash.html',
-        './manifest.json',
-        './icons/icon-192x192.png',
-        './icons/icon-512x512.png'
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[Service Worker] Caching files');
+      return cache.addAll(URLS_TO_CACHE);
     })
   );
-  self.skipWaiting();
 });
 
+// Activate Service Worker and remove old caches
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        keys.map((key) => {
-          if (key !== 'star-cache-v2') {
-            return caches.delete(key);
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            console.log('[Service Worker] Deleting old cache:', name);
+            return caches.delete(name);
           }
         })
       );
     })
   );
-  self.clients.claim();
 });
 
+// Fetch from cache first, fallback to network
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request).catch(() => {
-        // If offline and request is for an HTML page, return index.html
+      // Return cached file or fetch from network
+      return response || fetch(event.request).then((fetchResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          // Cache new files for offline use
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      }).catch(() => {
+        // Fallback page if offline and file not cached
         if (event.request.mode === 'navigate') {
           return caches.match('./index.html');
         }
