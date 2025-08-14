e110d1594c274b40de1525ef2a948eb2b6b6a3a6
piupin <piupin0000@gmail.com>
2025-08-15 01:41:08 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -11,12 +11,36 @@ self.addEventListener('install', (event) => {
       ]);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== 'star-cache-v2') {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).catch(() => {
+        // If offline and request is for an HTML page, return index.html
+        if (event.request.mode === 'navigate') {
+          return caches.match('./index.html');
+        }
+      });
     })
   );
 });
